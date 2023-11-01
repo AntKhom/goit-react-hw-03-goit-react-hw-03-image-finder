f8d0664813869f3d004a474e3e9c54be11cfd91c
AntKhom <antonhomenko1988@gmail.com>
2023-11-01 15:29:52 -0400

    ready

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -3,34 +3,62 @@ import ImageGalleryItem from "./ImageGalleryItem";
 import Loader from "./Loader";
 import { fetchPictures } from "api/api";
 import css from "../css/imageGallery.module.css"
+import LoadMoreBtn from "./LoadMoreBtn";
 
 
 
 export default class ImageGallery extends Component {
     state = {
-        pictures: null,
+        pictures: [],
         error: null,
         status: 'idle',
-    }
+        page: 1,
+        totalHits: 0,
+    };
 
     componentDidUpdate(prevProps, prevState) {
         const prevQuery = prevProps.query;
         const newQuery = this.props.query;
         if (prevQuery !== newQuery) {
             console.log(newQuery);
-
-            this.setState({status:'pending'});
-
-                fetchPictures(newQuery,1).then(pictures => {
-                    console.log(pictures.hits);
-                    this.setState({ pictures: pictures.hits, status:'resolved' });
-                })
-                .catch(error => {
-                    this.setState({ error, status: 'error' });
-                })
+            this.setState({pictures: []});
+            this.updateGallery(newQuery)
+            this.setState({
+                status: 'pending',
+                page: 1,
+            });
         }
-    }
+
+        if (newQuery === prevQuery && this.state.page !== prevState.page) {
+            this.updateGallery(prevQuery);
+        }
+    };
     
+    updateGallery(query) {
+        fetchPictures(query,this.state.page).then(pictures => {
+            console.log(pictures.hits);
+            this.setState({
+                pictures: [...this.state.pictures,...pictures.hits],
+                status: 'resolved',
+                totalHits: pictures.totalHits,
+            });
+        })
+        .catch(error => {
+            this.setState({ error, status: 'error' });
+        })
+    }
+
+    hideLoadMoreButton = (totalHits, arr) => {
+        console.log(totalHits, arr.length);
+       return totalHits > arr.length;
+    }
+
+
+    loadMoreHandler = () => {
+        this.setState((prevState) => ({
+            page: prevState.page + 1
+        }));
+    };
     
     render() {
         const { error, pictures, status } = this.state;
@@ -48,16 +76,21 @@ export default class ImageGallery extends Component {
         }
         
         if (status === 'resolved') {
-            return <ul className={css.ImageGallery}>
-                {pictures.map(picture =>
-                    <ImageGalleryItem
-                        key={picture.id}
-                        imgUrl={picture.webformatURL}
-                        imgBigUrl={picture.largeImageURL}
-                        onClick={()=>this.props.openModal(picture.largeImageURL)}
-                    />
-                )}
-            </ul>
+            return (
+                <div>
+                    <ul className={css.ImageGallery}>
+                        {pictures.map(picture =>
+                            <ImageGalleryItem
+                                key={picture.id}
+                                imgUrl={picture.webformatURL}
+                                imgBigUrl={picture.largeImageURL}
+                                onClick={()=>this.props.openModal(picture.largeImageURL)}
+                            />
+                        )}
+                    </ul>
+                    {this.hideLoadMoreButton(this.state.totalHits,this.state.pictures)&&<LoadMoreBtn onClick={this.loadMoreHandler}/>}
+                </div>
+            )     
         }
     }
 }
